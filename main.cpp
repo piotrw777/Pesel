feb6f7c94edc43686f4ec33642fa4a466592f838
piotrw777 <piotr.wasilewski2@gmail.com>
2021-03-05 21:14:09 +0100

    coraz bliżej święta

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "person.h"
 #include "date.h"
 #include "pom_functions.h"
+#include <map>
 
 using namespace std;
 
@@ -46,13 +47,20 @@ int main(int argc,  char * argv[] )
     w1 = thread(hello, 5, "hi!");
     w1.join();
 
-    vector<int> arguments(argc - 1, 0);
+    vector<int> arguments(argc - 2, 0);
     const int M = atoi(argv[1]);
+
     vector<shared_ptr<Person> > persons;
-    vector<shared_ptr<Person> > persons_name;
+
+    vector<shared_ptr<Person> > persons_pesel; //0
+    vector<shared_ptr<Person> > persons_name;  //1
+    vector<shared_ptr<Person> > persons_surname; //2
+    vector<shared_ptr<Person> > persons_address; //3
+    vector<shared_ptr<Person> > persons_age;    //4
+    vector<shared_ptr<Person> > persons_date_of_birth; //5
 
     for (size_t k = 0; k < arguments.size()  ; k++) {
-        arguments[k] = atoi(argv[k + 1]);
+        arguments[k] = atoi(argv[k + 2]);
     }
 
     //0 - pesel
@@ -63,17 +71,40 @@ int main(int argc,  char * argv[] )
     //5 - date urodzenia
 
     cout << "Przeslane argumenty: \n";
-    for(const &v : arguments) {
+    for(const auto &v : arguments) {
         cout << v << " ";
     }
-    cout << endl;
 
+    cout << endl;
+    //tworzenie listy osób
     for(int k = 0; k < M; k++) {
         persons.push_back(make_shared<Person>(Random{}));
     }
-    persons_name=persons;
-
+    //tworzymy shared pointery
+    persons_date_of_birth=persons_age=persons_address=persons_surname=persons_name=persons_pesel=persons;
+    //sortujemy
+    persons_sort(persons_pesel, &Person::get_pesel);
     persons_sort(persons_name, &Person::get_name);
+    persons_sort(persons_surname, &Person::get_surname);
+    persons_sort(persons_age, &Person::get_age);
+    //persons_sort(persons_date_of_birth, &Person::get_date_of_birth);
+    //napisać funkcję mniejszości dla daty urodzenia
+
+    int nr_zad = 1;
+    int start_position;
+    int list_len;
+    int case_nr;
+
+    for(size_t k = 0; k < arguments.size() ; k += 3) {
+        cout << "Zadanie nr " << nr_zad++ << endl;
+        case_nr = arguments[k];
+        start_position = arguments[k + 1];
+        list_len = arguments[k + 2];
+        switch (case_nr) {
+
+        }
+        cout << arguments[k] <<" " << arguments[k+1] << " " << arguments[k+2] << endl;
+    }
 
     print(persons);
     print(persons_name);
